bfacaeaabc6996b4a9048cc6176e423580a438b9
eurico77 <euricomagalhaes61@gmail.com>
2024-06-09 13:23:25 -0300

    feat: create account repository implementation

---
+++ backend/src/infra/repositories/AccountRepository.ts
@@ -0,0 +1,22 @@
+import AccountsRepository from '../../application/repositories/AccountRepository';
+import Account from '../../domain/Account';
+import { KnexDatabase } from '../database/knex/KnexDatabase';
+
+export default class KnexAccountsRepository implements AccountsRepository {
+  private knex = KnexDatabase.getInstance();
+
+  async create(account: Account): Promise<void> {
+    await this.knex('accounts').insert({
+      accountId: account.accountId,
+      name: account.name.value,
+      email: account.email.value,
+      password: account.password.value,
+    });
+  }
+
+  async findByEmail(email: string): Promise<Account | undefined> {
+    const result = await this.knex('accounts').where({ email }).first();
+    if (!result) return undefined;
+    return result;
+  }
+}
