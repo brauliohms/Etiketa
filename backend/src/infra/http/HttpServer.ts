e4a42223420e06d099cf4f42eb5dc618791e1f43
eurico77 <euricomagalhaes61@gmail.com>
2024-06-09 13:21:58 -0300

    feat: alter contract httpServer port

--- backend/src/infra/http/HttpServer.ts
+++ backend/src/infra/http/HttpServer.ts
@@ -1,4 +1,10 @@
 export default interface HttpServer {
-  register(method: string, url: string, callback: Function): void;
+  register(
+    method: string,
+    url: string,
+    callback: Function,
+    middlewares?: any[]
+  ): void;
   listen(port: number): void;
+  setupSwagger(swaggerFilePath: string): void;
 }
