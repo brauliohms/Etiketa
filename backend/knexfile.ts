234c56dc932437b6df11ae941563c132ba49fdce
eurico77 <euricomagalhaes61@gmail.com>
2024-06-09 13:17:36 -0300

    feat: add knex query builder and configure

---
+++ backend/knexfile.ts
@@ -0,0 +1,43 @@
+import 'dotenv/config';
+import type { Knex } from 'knex';
+import path from 'path';
+
+const { DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD, DATABASE_NAME } =
+  process.env;
+
+if (!DATABASE_HOST || !DATABASE_USER || !DATABASE_PASSWORD || !DATABASE_NAME)
+  throw new Error('One or more required environment variables are missing');
+
+const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'pg' as string,
+    connection: {
+      host: DATABASE_HOST,
+      user: DATABASE_USER,
+      password: DATABASE_PASSWORD,
+      database: DATABASE_NAME,
+    },
+    migrations: {
+      directory: path.resolve(
+        __dirname,
+        'src',
+        'infra',
+        'database',
+        'knex',
+        'migrations'
+      ),
+    },
+    seeds: {
+      directory: path.resolve(
+        __dirname,
+        'src',
+        'infra',
+        'database',
+        'knex',
+        'seeds'
+      ),
+    },
+  },
+};
+
+export default config;
