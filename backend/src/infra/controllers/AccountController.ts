c00b0980bd9f88ea85a87158527edefa06346395
eurico77 <euricomagalhaes61@gmail.com>
2024-06-09 13:20:36 -0300

    feat: create authenticate usecase

--- backend/src/infra/controllers/AccountController.ts
+++ backend/src/infra/controllers/AccountController.ts
@@ -1,28 +1,37 @@
 import CreateAccountUseCase from '../../application/usecases/CreateAccount';
-import GetAccountUseCase from '../../application/usecases/GetAccount';
+import GetAccountUseCase from '../../application/usecases/Authenticate';
 import HttpServer from '../http/HttpServer';
+import { AuthMiddleware } from '../http/middlewares/AuthMiddleware';
 
 export class AccountController {
   constructor(
     private httpServer: HttpServer,
     private accountUseCase: CreateAccountUseCase,
-    private getAccountUseCase: GetAccountUseCase
+    private getAccountUseCase: GetAccountUseCase,
+    private authMiddleware: AuthMiddleware
   ) {
     this.httpServer.register(
       'post',
-      '/accounts',
+      '/signup',
       async (params: any, body: any) => {
         await this.accountUseCase.execute(body);
       }
     );
     this.httpServer.register(
-      'get',
-      '/accounts',
+      'post',
+      '/signin',
       async (params: any, body: any) => {
-        const { email } = body;
-        console.log(email);
-        return await this.getAccountUseCase.execute(email);
+        const { email, password } = body;
+        return await this.getAccountUseCase.execute(email, password);
       }
+    );
+    this.httpServer.register(
+      'get',
+      '/protected',
+      async (params: any, body: any) => {
+        return { ok: true };
+      },
+      [this.authMiddleware]
     );
   }
 }
