14ba205bdc5dde83ceb759391c7c2668b7ce5992
Braulio Henrique Marques Souto <braulio@disroot.org>
2024-06-14 09:35:59 -0300

    Change cors position to file start and put the simple cors config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,33 +1,41 @@
-import 'dotenv/config';
-import cors from 'cors';
-import path from 'node:path';
-import CreateAccountUseCase from './application/usecases/CreateAccount';
-import ExpressAdapter from './infra/http/ExpressHttpAdapter';
-import KnexAccountsRepository from './infra/repositories/AccountRepository';
-import JwtTokenService from './infra/utils/JsonWebToken';
-import BcryptPasswordHasher from './infra/utils/Bcrypt';
-import AuthenticateUseCase from './application/usecases/Authenticate';
-import AccountController from './infra/controllers/AccountController';
-import AuthMiddleware from './infra/http/middlewares/AuthMiddleware';
-import EnvChecker from './infra/configs/EnvsChecker';
-import { KnexTagsRepository } from './infra/repositories/TagRepository';
-import CreateTagUseCase from './application/usecases/CreateTag';
-import TagController from './infra/controllers/TagController';
-import GetTagUseCase from './application/usecases/GetTagById';
+import cors from "cors";
+import "dotenv/config";
+import path from "node:path";
+import AuthenticateUseCase from "./application/usecases/Authenticate";
+import CreateAccountUseCase from "./application/usecases/CreateAccount";
+import CreateTagUseCase from "./application/usecases/CreateTag";
+import GetTagUseCase from "./application/usecases/GetTagById";
+import EnvChecker from "./infra/configs/EnvsChecker";
+import AccountController from "./infra/controllers/AccountController";
+import TagController from "./infra/controllers/TagController";
+import ExpressAdapter from "./infra/http/ExpressHttpAdapter";
+import AuthMiddleware from "./infra/http/middlewares/AuthMiddleware";
+import KnexAccountsRepository from "./infra/repositories/AccountRepository";
+import { KnexTagsRepository } from "./infra/repositories/TagRepository";
+import BcryptPasswordHasher from "./infra/utils/Bcrypt";
+import JwtTokenService from "./infra/utils/JsonWebToken";
 
 EnvChecker.checkEnvVariables([
-  'PORT',
-  'JWT_SECRET',
-  'DATABASE_USER',
-  'DATABASE_PASSWORD',
-  'DATABASE_CLIENT',
-  'DATABASE_HOST',
-  'DATABASE_NAME',
-  'NODE_ENV',
+  "PORT",
+  "JWT_SECRET",
+  "DATABASE_USER",
+  "DATABASE_PASSWORD",
+  "DATABASE_CLIENT",
+  "DATABASE_HOST",
+  "DATABASE_NAME",
+  "NODE_ENV",
 ]);
 
 // infra bootstrap
 const expressHttpServer = new ExpressAdapter();
+// server cors
+expressHttpServer.use(
+  cors()
+  // cors({
+  //   origin: "*",
+  //   optionsSuccessStatus: 200,
+  // })
+);
 const jwtAuth = new JwtTokenService();
 const bcryptHasher = new BcryptPasswordHasher();
 const authMiddleware = new AuthMiddleware(jwtAuth);
@@ -61,17 +69,11 @@ new TagController(
 // documentation bootstrap
 const swaggerFilePath = path.resolve(
   __dirname,
-  'infra',
-  'configs',
-  'swagger',
-  'api.json'
+  "infra",
+  "configs",
+  "swagger",
+  "api.json"
 );
 expressHttpServer.setupSwagger(swaggerFilePath);
 
-// server bootstrap
-expressHttpServer.use(
-  cors({
-    origin: '*',
-  })
-);
 expressHttpServer.listen(Number(process.env.PORT));
